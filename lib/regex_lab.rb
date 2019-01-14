bcfd9fde17cb4ab0cf495c12b2ea87023496cdb7
Steve Albonico <salbonico@gmail.com>
2019-01-14 19:13:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,8 @@
 def starts_with_a_vowel?(word)
   array = word.scan(/\s[aeiouAEIOU]/)
-  if array.count > 0
-    return array
-  else return array
+  if array != nil
+    return true
+  else return false
   end
 end
 
