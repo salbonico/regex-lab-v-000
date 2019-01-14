474f8c8cf9bfbc109af38007fc280babc867ee77
Steve Albonico <salbonico@gmail.com>
2019-01-14 20:20:38 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  array = word.scan(/\b[aeiouAEIOU]/)
+  if array.length > 0
+    return true
+  else return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  array = text.scan(/\bun\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  array = text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+ array = text.scan(/\b[A-Z]\w+[.!?\\-\\,]/)
+ if array.length > 0
+   return true
+ else return false
+ end
 end
 
 def valid_phone_number?(phone)
-
+array = phone.scan(/[\\(]?[0-9]{3}[\\)]?\s?[0-9]{3}\s?[\\-]?[0-9]{4}/)
+if array.length > 0
+  return true
+else return false
+end
 end
