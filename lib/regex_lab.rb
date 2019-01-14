b0ea3e682a017ff3eee38de4b8cc3f25747d6b62
Steve Albonico <salbonico@gmail.com>
2019-01-14 20:19:17 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-array = phone.scan(//)
+array = phone.scan(/[\\(]?[0-9]{3}[\\)]?\s?[0-9]{3}\s?[\\-]?[0-9]{4}/)
 end
