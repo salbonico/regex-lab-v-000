ea8f34836cfa58e5cb40f3ec94b77f122b7c96b9
Steve Albonico <salbonico@gmail.com>
2019-01-14 19:06:41 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.scan(/^[aeiouAEIOU]/)
+  array = word.scan(/\s[aeiouAEIOU]/)
+  if array.length > 0
+    return true
+  else return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
