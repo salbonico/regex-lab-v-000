a7a2c87c1ddf21f1684e1427fc3dcc02359c50b5
Steve Albonico <salbonico@gmail.com>
2019-01-14 20:01:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  array = text.scan(/\bun*ing\s/)
+  array = text.scan(/\bun\w*ing\s/)
 end
 
 def words_five_letters_long(text)
@@ -23,5 +23,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+array = phone.scan(//)
 end
